0565ff3ef6d04960d600f05b499b9ec94dddded3
speedemissoes-bit <speed.emissoes@gmail.com>
2026-06-03 09:45:12 -0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/speedmilhas/',
+  define: {
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
+    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY),
+  }
+})
